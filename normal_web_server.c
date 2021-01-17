a93075ba59dcb692ed0df056a15533fc05b06c56
Austen Mack <austen.mack@asu.edu>
2021-01-16 22:19:38 -0700

    added 404 response header

--- normal_web_server.c
+++ normal_web_server.c
@@ -5,4 +5,18 @@ int main() {
 
     printf("\nHello, world!\n\n");
 
+    static char* not_found_response_template =
+        "HTTP/1.1 404 Not Found\r\n"
+        "Content-type: text/html\r\n"
+        "\r\n"
+        "<html>\r\n"
+        " <body>\r\n"
+        "  <h1>Not Found</h1>\r\n"
+        "  <p>The requested URL was not found on this server.</p>\r\n"
+        " </body>\r\n"
+        "</html>\r\n";
+
+    //len = strlen(not_found_response_template);
+    //send(newSct, not_found_response_template, len, 0);
+
 };
