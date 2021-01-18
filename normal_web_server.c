cebda7b5bdef6892b55a060ffceb7e55868d7f86
Austen Mack <austen.mack@asu.edu>
2021-01-17 20:06:09 -0700

    implements socket, connects but not responding

--- normal_web_server.c
+++ normal_web_server.c
@@ -3,6 +3,7 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
 
@@ -28,24 +29,37 @@ int main(int argc, char *argv[]) {
 
     bind(mah_socket, (struct sockaddr *) &server_address, sizeof(server_address));
 
-    listen(mah_socket, 0);    
+    listen(mah_socket, 3);    
     
     //GET requests of valid URL format get 404
     //GET requests with URL of form /exec/<command> execute it using libc function
     //HTTP response is stdout of executed command, status code 200
     //No limit on characters, be able to handle anything
 
-    static char* not_found_response_template =
+    /*static char* not_found_response_template =
         "HTTP/1.1 404 Not Found\r\n"
-        "Content-type: text/html\r\n"
+        "Content-Type: text/html\r\n"
         "\r\n"
         "<html>\r\n"
         " <body>\r\n"
         "  <h1>Not Found</h1>\r\n"
-        "  <p>The requested URL was not found on this server.</p>\r\n"
+        "  <p>The requested URL was not found on this server. Frig off!!</p>\r\n"
         " </body>\r\n"
-        "</html>\r\n";
+        "</html>\r\n";*/
 
+    static char* test =
+        "HTTP/1.1 404 Not Found\r\n"
+        "Content-Type: text/html\r\n"
+        "<html><body> Giving it a test here. </body></html>";
+
+    int client_socket;
+    while(1) {
+        client_socket = accept(client_socket, NULL, NULL);
+        send(client_socket, test, sizeof(test), 0);
+        close(client_socket);
+    }
+
+    return 0;
     //len = strlen(not_found_response_template);
     //send(newSct, not_found_response_template, len, 0);
 
